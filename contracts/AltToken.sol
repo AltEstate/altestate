9784e6adbc8b483d493aa8dc1d05a6912969376b
Aler Denisov <aler.zampillo@gmail.com>
2018-03-15 14:01:13 +0300

    alt token name

--- contracts/AltToken.sol
+++ contracts/AltToken.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.4.18;
 import './base/DefaultToken.sol';
 
 contract AltToken is DefaultToken {
-  function AltToken(address _registry) DefaultToken("Alt Estate", "ALT", 18, _registry) public {
+  function AltToken(address _registry) DefaultToken("ALT test", "ALTt", 18, _registry) public {
   }
 }
--- merged/AltCrowdsalePhases.sol
+++ merged/AltCrowdsalePhases.sol
@@ -1056,8 +1056,8 @@ contract AltCrowdsalePhaseTwo is BaseAltCrowdsale {
     // Whitelisted
     false,
 
-    // price 1 ETH -> 100 ALT
-    uint(1 ether).div(100), 
+    // price 1 ETH -> 100000 ALT
+    uint(1 ether).div(100000), 
 
     // start
     block.timestamp + 60 days,
@@ -1090,7 +1090,7 @@ contract AltCrowdsalePhaseOne is BaseAltCrowdsale {
     false,
 
     // price 1 ETH -> 200 ALT
-    uint(1 ether).div(200), 
+    uint(1 ether).div(100000), 
 
     // start
     block.timestamp,
