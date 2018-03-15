a671a6a23ad00b2947c317c25d2cf8d36c0e1b1d
Aler Denisov <aler.zampillo@gmail.com>
2018-03-15 14:00:03 +0300

    prices and test deadlines

--- contracts/AltCrowdsalePhases.sol
+++ contracts/AltCrowdsalePhases.sol
@@ -2,39 +2,6 @@ pragma solidity ^0.4.18;
 
 import './base/BaseAltCrowdsale.sol';
 
-contract AltCrowdsalePhaseTwo is BaseAltCrowdsale {
-  function AltCrowdsalePhaseTwo(
-    address _registry,
-    address _token,
-    address _extraTokensHolder,
-    address _wallet
-  )
-  BaseAltCrowdsale(
-    _registry,
-    _token,
-    _extraTokensHolder,
-    _wallet,
-
-    // Whitelisted
-    false,
-
-    // price 1 ETH -> 100000 ALT
-    uint(1 ether).div(100000), 
-
-    // start
-    block.timestamp + 60 days,
-    // end 
-    block.timestamp + 90 days,
-
-    // _softCap,
-    0,
-    // _hardCap
-    15000 ether
-  ) 
-  public {
-  } 
-}
-
 contract AltCrowdsalePhaseOne is BaseAltCrowdsale {
   function AltCrowdsalePhaseOne (
     address _registry,
@@ -57,13 +24,46 @@ contract AltCrowdsalePhaseOne is BaseAltCrowdsale {
     // start
     block.timestamp,
     // end 
-    block.timestamp + 10 days,
+    block.timestamp + 1 days,
 
     // _softCap,
     0,
     // _hardCap
-    1500 ether
+    100 ether
   ) 
   public {
   } 
 } 
+
+contract AltCrowdsalePhaseTwo is BaseAltCrowdsale {
+  function AltCrowdsalePhaseTwo(
+    address _registry,
+    address _token,
+    address _extraTokensHolder,
+    address _wallet
+  )
+  BaseAltCrowdsale(
+    _registry,
+    _token,
+    _extraTokensHolder,
+    _wallet,
+
+    // Whitelisted
+    false,
+
+    // price 1 ETH -> 100000 ALT
+    uint(1 ether).div(100000), 
+
+    // start
+    block.timestamp + 1 days,
+    // end 
+    block.timestamp + 2 days,
+
+    // _softCap,
+    0,
+    // _hardCap
+    15000 ether
+  ) 
+  public {
+  } 
+}
