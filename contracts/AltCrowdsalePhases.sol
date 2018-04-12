b38be76ea58f378a4762fac698d148ff3a81472a
as-popov <36487435+as-popov@users.noreply.github.com>
2018-04-12 16:04:48 +0300

    Update AltCrowdsalePhases.sol

--- contracts/AltCrowdsalePhases.sol
+++ contracts/AltCrowdsalePhases.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.18;
 
 import './base/BaseAltCrowdsale.sol';
 
-contract AltCrowdsalePhaseOne is BaseAltCrowdsale {
-  function AltCrowdsalePhaseOne (
+contract demoAltCrowdsalePhaseOne is BaseAltCrowdsale {
+  function demoAltCrowdsalePhaseOne (
     address _registry,
     address _token,
     address _extraTokensHolder,
@@ -24,46 +24,13 @@ contract AltCrowdsalePhaseOne is BaseAltCrowdsale {
     // start
     block.timestamp,
     // end 
-    block.timestamp + 1 days,
+    block.timestamp + 90 days,
 
     // _softCap,
     0,
     // _hardCap
-    100 ether
+    1000000 ether
   ) 
   public {
   } 
 } 
-
-contract AltCrowdsalePhaseTwo is BaseAltCrowdsale {
-  function AltCrowdsalePhaseTwo(
-    address _registry,
-    address _token,
-    address _extraTokensHolder,
-    address _wallet
-  )
-  BaseAltCrowdsale(
-    _registry,
-    _token,
-    _extraTokensHolder,
-    _wallet,
-
-    // Whitelisted
-    false,
-
-    // price 1 ETH -> 100000 ALT
-    uint(1 ether).div(100000), 
-
-    // start
-    block.timestamp + 1 days,
-    // end 
-    block.timestamp + 2 days,
-
-    // _softCap,
-    0,
-    // _hardCap
-    15000 ether
-  ) 
-  public {
-  } 
-}
